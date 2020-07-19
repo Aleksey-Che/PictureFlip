6598427256489595f12737d6a51c31c9f3c1cec0
Aleksey-Che <awesomelold@mail.ru>
2020-07-19 15:30:03 +0400

    homework#2

---
+++ PictureFlip/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PictureFlip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dirName = Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(Directory.GetCurrentDirectory())))) + "\\testPics";
+
+            var files = Directory.GetFiles(dirName, "*.*").Where(str => str.EndsWith(".jpg") || str.EndsWith(".png") || str.EndsWith(".bmp"));
+
+            var sw = new Stopwatch();
+
+            Parallel.ForEach(files, (currentFile) =>
+            {
+                sw.Start();
+
+                string filename = Path.GetFileName(currentFile);
+                var bitmap = new Bitmap(currentFile);
+                bitmap.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                bitmap.Save(Path.Combine(dirName, filename));
+
+                sw.Stop();
+
+                Console.WriteLine($"Файл {filename} принят в обработку потоком №{Thread.CurrentThread.ManagedThreadId}");
+                Console.WriteLine($"Время выполнения операции: {sw.ElapsedMilliseconds} мс");
+            });
+
+            Console.WriteLine("Выполнение завершено успешно.");
+            Console.ReadKey();
+        }
+    }
+}
